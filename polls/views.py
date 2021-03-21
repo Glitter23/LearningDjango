f240e262501586dadffd3cb3e2ad91f09f752afa
Glitter23 <rashmikirti@rocketmail.com>
2021-03-21 23:51:27 +0530

    poll app

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,59 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-
+from django.template import Context, loader
+import  os
 # Create your views here.
+
+from django.utils import timezone
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+
+from .models import Choice, Question
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+    def get_queryset(self):
+
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+
+
+
+from django.http import HttpResponse
+
+from .models import Question
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+
